695dc9b22bf9d7ec136f72bdff8aca0170416cc5
Ruslan Tolstov <tolstov@mlsdev.com>
2020-04-25 15:48:45 +0300

    iOS client FCM

---
+++ .clients/ios_fcm/activepush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  activepush
+//
+//  Created by Руслан Толстов on 25.04.2020.
+//  Copyright © 2020 ruslant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
